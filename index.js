a089b7b07fa06f7da3bbddcce11801be6012baf6
Melissa Vaiden <melissavaiden@gmail.com>
2022-01-11 18:00:23 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const employee = {
     streetAddress: "111 5th Ave",
 };
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    employee.name = "Jimmy";
+    const updatedEmployee = {...employee};
+    updatedEmployee[key] = value
+    return updatedEmployee;
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key]
+    return newEmployee
+    
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
     return employee;
 }
